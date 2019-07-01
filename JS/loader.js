1fd8fa458a84424ac1f58aabac6f2a71ed0d693b
ojasiiitd <ojassaxena03@gmail.com>
2019-07-01 22:10:50 +0530

    Added X-movement

--- JS/loader.js
+++ JS/loader.js
@@ -6,7 +6,7 @@ var win = window,
     height = win.innerHeight || htm.clientHeight || body.clientHeight;
 
 var canvas = document.getElementsByTagName("canvas")[0];
-canvas.width = width-16;
+canvas.width = width-18;
 canvas.height = 800;
 var ctx = canvas.getContext("2d");
 
@@ -22,19 +22,21 @@ canvas.addEventListener("click" , pointerBall);
 var colors = [
     "tomato" , "blueviolet" , 
     "lime" , "snow" , 
-    "brown" , "black" ,
-    "orange" ,  "tan"
+    "orange"
     ];
-var xPos = [] , yPos = [] ,
+
+var ballColor = [] , xPos = [] , yPos = [] ,
     xVel = [] , yVel = [] , u = [] ,
     runtime = [] , g = 9.81 , restitution = 0.99 ,
-    run , runInterval = 1 ;
+    run , runInterval = 7 , radius = 10 , initialX;
 
 function pointerBall()
 {
+    initialX = getRandomInt(-5 , 5);
+    ballColor.push(colors[getRandomInt(0 , colors.length-1)]);
     xPos.push(event.clientX);
     yPos.push(event.clientY);
-    xVel.push(0);
+    xVel.push(initialX);
     yVel.push(0);
     u.push(0);
     runtime.push(0);
@@ -44,9 +46,11 @@ function pointerBall()
 
 function newBall()
 {
+    initialX = getRandomInt(-5 , 5);
+    ballColor.push(colors[getRandomInt(0 , colors.length-1)]);
     xPos.push(getRandomInt(13 , canvas.width-13));
     yPos.push(getRandomInt(13 , canvas.height-13));
-    xVel.push(0);
+    xVel.push(initialX);
     yVel.push(0);
     u.push(0);
     runtime.push(0);
@@ -69,17 +73,17 @@ box();
 function box()
 {
     ctx.lineWidth = 10;
-    ctx.strokeStyle = "red";
+    ctx.strokeStyle = "slateblue";
     ctx.strokeRect(0 , 0 , canvas.width , canvas.height);
 }
 
-function ball(x , y)
+function ball()
 {
-    ctx.fillStyle = "blueviolet";
-    for(var c = 0 ; c<x.length ; c++)
+    for(var c = 0 ; c<xPos.length ; c++)
     {
+        ctx.fillStyle = ballColor[c];
         ctx.beginPath();
-        ctx.arc(x[c] , y[c] , 14 , 0 , 2*Math.PI);
+        ctx.arc(xPos[c] , yPos[c] , 2*radius , 0 , 2*Math.PI);
         ctx.fill();
         ctx.closePath();
     }
@@ -99,14 +103,23 @@ function draw()
         runtime[it] += runInterval;
     for(var it = 0 ; it<yPos.length ; it++)
     {
+        xPos[it] += xVel[it];
         yPos[it] += yVel[it];
+
+        if(xVel[it] <= 0.3 && xVel[it] >= (-0.3))
+            xVel[it] = 0;
+        if(yVel[it] <= 0.3 && yVel[it] >= (-0.3))
+            yVel[it] = 0;
 
         if(yVel[it] >= 0 && u[it] == 0)
             yVel[it] = (g*(runtime[it]/1000));
         else
             yVel[it] = u[it] + (g*(runtime[it]/1000));
         
-        if(yPos[it]+19 >= canvas.height)
+        if(xPos[it]+12+radius >= canvas.width || xPos[it]-12-radius <= 0)
+            xVel[it] *= (-1);
+
+        if(yPos[it]+12+radius >= canvas.height)
         {
             runtime[it] = 0;
             yVel[it] *= (-1);
