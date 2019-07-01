4bc1f36578e5522eda5c9ed09e3920090f465ca8
ojasiiitd <ojassaxena03@gmail.com>
2019-07-01 17:36:34 +0530

    Organising code

--- JS/loader.js
+++ JS/loader.js
@@ -6,10 +6,18 @@ var win = window,
     height = win.innerHeight || htm.clientHeight || body.clientHeight;
 
 var canvas = document.getElementsByTagName("canvas")[0];
-canvas.width = width-30;
+canvas.width = width-16;
 canvas.height = 800;
-
 var ctx = canvas.getContext("2d");
+
+const addBall = document.querySelectorAll(".button")[0];
+const grav = document.querySelectorAll(".button")[1];
+const stop = document.querySelectorAll(".button")[2];
+
+addBall.addEventListener("click" , newBall);
+grav.addEventListener("click" , start);
+stop.addEventListener("click" , pause);
+canvas.addEventListener("click" , pointerBall);
 
 var colors = [
     "tomato" , "blueviolet" , 
@@ -17,89 +25,10 @@ var colors = [
     "brown" , "black" ,
     "orange" ,  "tan"
     ];
-
-box();
-
-function box()
-{
-    ctx.lineWidth = 10;
-    ctx.strokeStyle = "red";
-    ctx.strokeRect(0 , 0 , canvas.width , canvas.height);
-}
-
 var xPos = [] , yPos = [] ,
     xVel = [] , yVel = [] , u = [] ,
-    g = 9.81 , run , runtime = [] , runInterval = 1;
-
-function ball(x , y)
-{
-    ctx.fillStyle = "blueviolet";
-    for(var c = 0 ; c<x.length ; c++)
-    {
-        ctx.beginPath();
-        ctx.arc(x[c] , y[c] , 14 , 0 , 2*Math.PI);
-        ctx.fill();
-        ctx.closePath();
-    }
-}
-
-function drawInit()
-{
-    ctx.clearRect(0 , 0 , canvas.width , canvas.height);
-    box();
-
-    ball(xPos , yPos);   
-}
-
-function draw()
-{
-    for(var it = 0 ; it<yPos.length ; it++)
-        runtime[it] += runInterval;
-    for(var it = 0 ; it<yPos.length ; it++)
-    {
-        yPos[it] += yVel[it];
-
-        if(u[it] < 0 && (yVel[it] < 0.00000001 && yVel[it] > -0.00000001))
-        {
-            runtime[it] = 0;
-            yVel[it] = 0;
-            u[it] = yVel[it];
-        }
-
-        if(yVel[it] >= 0 && u[it] == 0)
-            yVel[it] = (g*(runtime[it]/1000));
-        else
-            yVel[it] = u[it] + (g*(runtime[it]/1000));
-        
-        if(yPos[it]+17 >= canvas.height)
-        {
-            runtime[it] = 0;
-            yVel[it] *= -.99;
-            u[it] = yVel[it];
-        }
-    }
-
-    drawInit();
-}
-
-function getRandomInt(min, max)
-{
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function getRandomFloat(min, max)
-{
-    return Math.random() * (max - min + 1) + min;15
-}
-
-const addBall = document.querySelectorAll(".button")[0];
-const grav = document.querySelectorAll(".button")[1];
-const stop = document.querySelectorAll(".button")[2];
-addBall.addEventListener("click" , newBall);
-grav.addEventListener("click" , start);
-stop.addEventListener("click" , pause);
-
-canvas.addEventListener("click" , pointerBall);
+    runtime = [] , g = 9.81 , restitution = 0.99 ,
+    run , runInterval = 1 ;
 
 function pointerBall()
 {
@@ -134,4 +63,67 @@ function start()
 function pause()
 {
     clearInterval(run);
+}
+
+box();
+function box()
+{
+    ctx.lineWidth = 10;
+    ctx.strokeStyle = "red";
+    ctx.strokeRect(0 , 0 , canvas.width , canvas.height);
+}
+
+function ball(x , y)
+{
+    ctx.fillStyle = "blueviolet";
+    for(var c = 0 ; c<x.length ; c++)
+    {
+        ctx.beginPath();
+        ctx.arc(x[c] , y[c] , 14 , 0 , 2*Math.PI);
+        ctx.fill();
+        ctx.closePath();
+    }
+}
+
+function drawInit()
+{
+    ctx.clearRect(0 , 0 , canvas.width , canvas.height);
+    box();
+
+    ball(xPos , yPos);   
+}
+
+function draw()
+{
+    for(var it = 0 ; it<yPos.length ; it++)
+        runtime[it] += runInterval;
+    for(var it = 0 ; it<yPos.length ; it++)
+    {
+        yPos[it] += yVel[it];
+
+        if(yVel[it] >= 0 && u[it] == 0)
+            yVel[it] = (g*(runtime[it]/1000));
+        else
+            yVel[it] = u[it] + (g*(runtime[it]/1000));
+        
+        if(yPos[it]+19 >= canvas.height)
+        {
+            runtime[it] = 0;
+            yVel[it] *= (-1);
+            yVel[it] *= (restitution);
+            u[it] = yVel[it];
+        }
+    }
+
+    drawInit();
+}
+
+function getRandomInt(min, max)
+{
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getRandomFloat(min, max)
+{
+    return Math.random() * (max - min + 1) + min;15
 }
